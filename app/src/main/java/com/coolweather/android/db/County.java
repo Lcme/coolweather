2c3f87af998c621d71453bea02e352352e93bdb4
luozhiwen <luozhiwen@booslink.cn>
2017-08-07 11:25:39 +0800

    省市县列表

--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -8,8 +8,8 @@ import org.litepal.crud.DataSupport;
 
 public class County extends DataSupport {
     private int id;
-    private String countuName;
-    private String weather;
+    private String countyName;
+    private String weatherId;
     private int cityId;
 
     public int getId() {
@@ -20,20 +20,20 @@ public class County extends DataSupport {
         this.id = id;
     }
 
-    public String getCountuName() {
-        return countuName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCountuName(String countuName) {
-        this.countuName = countuName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
-    public String getWeather() {
-        return weather;
+    public String getWeatherId() {
+        return weatherId;
     }
 
-    public void setWeather(String weather) {
-        this.weather = weather;
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
     }
 
     public int getCityId() {
